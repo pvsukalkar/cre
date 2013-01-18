1174f414e73c310953dc5accde460407cdee22d5
Pavan <pvsukalkar@iitj.ac.in>
2013-01-18 13:38:03 +0530

    second commit

---
+++ src/post/Evaluation.java
@@ -0,0 +1,82 @@
+package post;
+
+
+public class Evaluation{  
+   public float calculate(String s)  
+   {  
+	   String arr[]= s.split(" ");
+	   
+     int n;float r=0;  
+     n=s.length();  
+     Stack a=new Stack(n);  
+     for(int i=0;i<arr.length;i++)  
+     {  
+    	
+         
+       if(!check_ex(arr[i]))  
+         a.push(Float.parseFloat(arr[i]));  
+       else  
+       {  char ch=arr[i].charAt(0);
+         float x=a.pop();  
+         float y=a.pop();  
+         switch(ch)  
+         {  
+           case '+':r=x+y;  
+              break;  
+           case '-':r=y-x;  
+              break;  
+           case '*':r=x*y;  
+              break;  
+           case '/':r=y/x;  
+              break;  
+           default:r=0;  
+         }  
+         a.push(r);  
+       }  
+     }  
+     
+     r=a.pop();
+     if(a.empty()){
+    	 return(r);	 
+     }
+     else{
+    	 return (34097);
+     }
+       
+   }
+   
+  static  boolean check_ex(String s){
+	   if(s.equals("+")||s.equals("-")||s.equals("/")||s.equals("*"))
+		   return true;
+	   return false;
+   }
+ 
+  class Stack  
+{  
+   private float[] a;  
+   private int top;
+   private int m;
+   public Stack(int max)  
+   {  
+     m=max;  
+     a=new float[m];  
+     top=-1;  
+   }  
+   public void push(float key)  
+   {  
+     a[++top]=key;  
+   }  
+   public float pop()  
+   {  
+     return(a[top--]);  
+   }  
+   public boolean empty(){
+	   if(top == -1){
+		   return true;
+	   }
+	   else{
+        	return false;
+	   }
+   }
+}  
+}  
---
+++ src/post/post.java
@@ -0,0 +1,102 @@
+
+package post;
+
+public class post extends javax.swing.JFrame {
+
+    
+    public post() {
+        initComponents();
+    }
+
+ 
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Enter postfix expression with spaces");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Evaluate");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addContainerGap(153, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+              
+        
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        Evaluation obj = new Evaluation();
+        String input = jTextField1.getText();
+        float ans = obj.calculate(input);
+        String ans1= ""+ans;
+        if (ans == 34097){
+            jLabel2.setText("Invalid Expression ");
+            jLabel2.show();
+        }   else{
+        jLabel2.setText(ans1);
+        jLabel2.show();
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+   
+    public static void main(String args[]) {
+      
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new post().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
